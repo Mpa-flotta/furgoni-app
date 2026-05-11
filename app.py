3afbb6fff2941baa6c10b8152da8805c8ddc464f
Mpa-flotta <a.borrelli@mpa-trasporti.it>
2026-05-11 16:48:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1513,12 +1513,6 @@ def uploaded_file(filename: str):
 # START APP
 # =========================
 
-init_db()
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-
-    app.run(
-        host="0.0.0.0",
-        port=port,
-    )
+    app.run(host="0.0.0.0", port=port)
